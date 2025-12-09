4875bbf441d50bc1daa632e447afa25f5a93c15a
Bhavya <bhavyaprataprathore.bca2025@spsu.ac.in>
2025-12-09 23:41:39 +0530

    moved checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='/checkout' element={<div>Testing</div>} />
+      <Route path='/checkout' element={<CheckoutPage />} />
     </Routes>
   )
 }
