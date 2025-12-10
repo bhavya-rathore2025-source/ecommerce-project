9351d5ecb0554be16ac9b6e091e41a05ccaa011d
Bhavya <bhavyaprataprathore.bca2025@spsu.ac.in>
2025-12-11 00:31:35 +0530

    Generating cart items using html

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import { CheckoutPage } from './pages/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
 import { NotFound } from './pages/NotFound'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 function App() {
+  const [cart, setCart] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/cart-items?expand=product').then((response) => {
+      setCart(response.data)
+    })
+  }, [])
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='checkout' element={<CheckoutPage />} />
+      <Route path='/' element={<HomePage cart={cart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
       <Route path='orders' element={<OrdersPage />} />
       <Route path='tracking' element={<TrackingPage />} />
       <Route path='*' element={<NotFound />} />
